041221db659a5007b0105ee7587de4e4954b0835
Jefferson Dutra <jells.dutra@gmail.com>
2026-01-08 16:02:32 -0300

    Atualização do site

---
+++ script.js
@@ -0,0 +1,86 @@
+// SELEÇÃO DE ELEMENTOS NO HTML 
+// const corpo = document.body
+//corpo.style.backgroundColor = "red"
+
+// const h1 = document.querySelector("h1")
+// h1.style.color = 'Grey'
+
+// const text = document.querySelector(".text")
+// text.style.color = 'Grey'
+// MUDAR TEXTO H1 - PARA MODIFICAR O QUE ESTÁ ESCRITO 
+// h1.innerText = "DESENVOLVIMENTO DE SISTEMAS"
+
+// FUNÇÕES E EVENTOS NO JS DOM - MUDAR TEMA FUNDO SITE
+// const tema = () => {
+//     const corpo = document.body
+//     corpo.style.backgroundColor = "green"
+
+// }
+
+// ATIVIDADE
+// DESENVOLVER AÇÕES ARTRAVÉS DE BOTÕES NA PÁGINA
+
+
+// // BOTÃO PARA MUDAR A COR DA FONTE DO TEXTO
+// const CorFonte = () => {
+//     const text = document.querySelector(".text")
+//     const h1 = document.querySelector("h1")
+//     h1.style.color = 'blue'
+//     text.style.color = 'silver'
+// }
+
+// // BOTÃO PARA MUDAR O BACKGRAUND DE UMA DIV
+// const CorDiv = () => {
+//     const corpo = document.querySelector(".text")
+//     text.style.backgroundColor = 'yellow'
+// }
+
+// // BOTÃO PARA ALTERAR O TITULO
+// const MudarTitulo = () => {
+//     const corpo = document.body
+//     h1.innerText = "Programador WEB - 2025/2026"
+// }
+
+// // BOTÃO PARA ALTERAR O TEXTO
+// const MudarTexto = () => {
+//     document.querySelector(".text")
+//     text.innerText = "Jefferson Dutra"
+// }
+
+// // BOTÃO PARA ALTERNAR O TEMA DA PÁGINA
+// const tema = () => {
+//     const corpo = document.body
+//     if (corpo.style.backgroundColor === "black") {
+//         corpo.style.backgroundColor = "white";
+//     } else {
+//         corpo.style.backgroundColor = "black";
+//     }
+// }
+
+
+
+
+// const exibirNome = () => {
+//     const texto = document.querySelector("texto")
+//     const meuNome = document.querySelector("#nome").value
+//     return texto.innerHTML = meuNome;
+// }
+
+// const exibirSenha = () => {
+//     const senha = documento.querySelector("#senha")
+//     //return senha.setAttribute("type", "text")
+
+//     if (senha.type === "password") {
+//         senha.type = "text"
+//     } else {
+//         senha.type = "password"
+//     }
+// }
+
+// const card = document.querySelector("#card")
+// const logEventos = document.querySelector("#logEventos")
+// card.style.backgroundColor = "green"
+
+// card.addEventListener("click", () => {
+//     return logEventos.innerHTML += "<br><b>Clicou</b>"
+// })
